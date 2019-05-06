b044e75ce80c882942c7ce589ecd651ce78ff88d
Yeison <yeison@resuelvetudeuda.com>
2019-05-05 19:55:26 -0500

    is alive

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,93 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
-import ArrowIcon from './Components/ArrowIcon';
+import {
+  ComboBox,
+  ComboBoxLabel,
+  ComboBoxInput,
+  ComboBoxMenu,
+  ComboBoxItem
+} from './ComboBox';
 
-const ComboBox: React.FC = ({ children }) => (
-  <div className='Combobox'>
-    {children}
-  </div>
-)
-
-const ComboBoxLabel: React.FC = ({ children }) => (
-  <div className='Combobox-label'>
-    {children}
-  </div>
-)
-
-const ComboBoxInput: React.FC = () => (
-  <div className="relative">
-    <input type='text' className='Combobox-input'/>
-    <div className='Combobox-button'>
-      <ArrowIcon />
-    </div>
-  </div>
-)
-
-const ComboBoxMenu: React.FC<{isOpen?: boolean}> = ({ children, isOpen = false }) => {
-  return (
-    <div className="relative">
-      <div className='Combobox-menu Combobox-menu__open'>
-        {isOpen ? children : null}
-      </div>
-    </div>
-  )
+interface ComboBoxState {
+  isOpen: boolean,
+  inputValue: string,
+  selectedItem: number | null
 }
 
-const ComboBoxItem: React.FC = ({ children }) => (
-  <div className='Combobox-item Combobox-item__selected'>{children}</div>
-)
+type Action = { type: "INPUT", payload: string }
+  | { type: "SELECT", payload: { id: number, label: string } }
+  | { type: "TOGGLE_MENU" }
+  | { type: "CLEAR" }
+
+const initialState: ComboBoxState = {
+  isOpen: false,
+  inputValue: '',
+  selectedItem: null
+}
+
+const comboBoxReducer = (
+  state: ComboBoxState = initialState,
+  action: Action
+): ComboBoxState => {
+  switch (action.type) {
+    case 'INPUT':
+      return {
+        ...state,
+        inputValue: action.payload,
+        isOpen: state.selectedItem ? !action.payload : !!action.payload
+      }
+    case 'SELECT':
+      return {
+        ...state,
+        inputValue: action.payload.label,
+        selectedItem: action.payload.id,
+        isOpen: false
+      }
+    case 'TOGGLE_MENU':
+      return {
+        ...state,
+        isOpen: !state.isOpen
+      }
+    case "CLEAR":
+      return initialState
+    default:
+      throw new Error('Action not valid');
+  }
+}
+
+const options = [{ id: 1, label: 'Mango' }, { id: 2, label: 'Manzana' }]
 
 const App: React.FC = () => {
+  const [state, dispatch] = useReducer(comboBoxReducer, initialState)
+
   return (
     <ComboBox>
       <ComboBoxLabel>Selecciona algo:</ComboBoxLabel>
-      <ComboBoxInput />
-      <ComboBoxMenu>
-        <ComboBoxItem>Option</ComboBoxItem>
+      <ComboBoxInput
+        value={state.inputValue}
+        onChange={(e) => dispatch({ type: 'INPUT', payload: e.target.value })}
+        toggleMenu={(e) => {
+          e.preventDefault()
+          dispatch({ type: "TOGGLE_MENU" })
+        }}
+        isOpen={state.isOpen}
+        selectedItem={state.selectedItem}
+        clear={() => dispatch({ type: 'CLEAR' })}
+      />
+      <ComboBoxMenu isOpen={state.isOpen}>
+        {
+          options
+            .filter(({ label }) => label.includes(state.inputValue))
+            .map(({ id, label }) => (
+              <ComboBoxItem
+                key={id}
+                onClick={() => dispatch({ type: 'SELECT', payload: { id, label } })}
+                isSelected={state.selectedItem === id}
+              >
+                {label}
+              </ComboBoxItem>
+            ))
+        }
       </ComboBoxMenu>
     </ComboBox>
   );
---
+++ src/ComboBox.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import ArrowIcon from './Components/ArrowIcon';
+import XIcon from './Components/XIcon';
+
+export const ComboBox: React.FC = ({ children }) => (
+  <div className='Combobox'>
+    {children}
+  </div>
+);
+
+export const ComboBoxLabel: React.FC = ({ children }) => (
+  <div className='Combobox-label'>
+    {children}
+  </div>
+);
+
+interface Input {
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  toggleMenu: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  clear: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  selectedItem: number | null;
+  isOpen: boolean;
+}
+
+export const ComboBoxInput: React.FC<Input> = ({
+  value,
+  onChange,
+  toggleMenu,
+  isOpen,
+  selectedItem,
+  clear
+}) => (
+    <div className="relative">
+      <input
+        type='text'
+        className={`Combobox-input ${isOpen ? "Combobox-input__open" : ''}`}
+        value={value}
+        onChange={onChange}
+      />
+      <div className='Combobox-button' onClick={(selectedItem && !isOpen) ? clear : toggleMenu}>
+        {(isOpen || selectedItem) ? <XIcon /> : <ArrowIcon />}
+      </div>
+    </div>
+  );
+
+interface Menu {
+  isOpen?: boolean;
+}
+export const ComboBoxMenu: React.FC<Menu> = ({ children, isOpen = false }) => {
+  return (<div className="relative">
+    <div className={`Combobox-menu ${isOpen ? 'Combobox-menu__open' : ''}`}>
+      {isOpen ? children : null}
+    </div>
+  </div>);
+};
+
+interface Item {
+  onClick: any;
+  isSelected: boolean;
+}
+export const ComboBoxItem: React.FC<Item> = ({ children, onClick, isSelected }) => (
+  <div className={`Combobox-item ${isSelected ? 'Combobox-item__selected' : ''}`} onClick={onClick}>
+    {children}
+  </div>
+);
